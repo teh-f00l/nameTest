f4dbb093e5e218153b926606a53d63bd4493adc3
teh-f00l <vihavira@protonmail.com>
2018-08-06 15:35:36 -0400

    here we are

---
+++ form.js
@@ -0,0 +1,6 @@
+function getFormValue(){
+    var fname=document.getElementById("fname").value; 
+    var lname=document.getElementById("lname").value;
+    document.getElementById("display").innerHTML= "HI " + fname + " " + lname;
+ 
+ }
